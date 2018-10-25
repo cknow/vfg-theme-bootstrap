1322a09f4d3888f626cbfafaa426c460f66576b8
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2018-10-25 09:19:23 -0300

    chore(eslint): update

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -4,6 +4,10 @@ module.exports = {
         '@cknow/eslint-config/plugins/jest'
     ],
     rules: {
+        'max-lines-per-function': 'off',
+        'max-lines': 'off',
+        'max-statements': ['error', { max: 50 }],
+        'newline-per-chained-call': ['error', { ignoreChainWithDepth: 5}],
         'require-jsdoc': 'off',
         'jest/prefer-expect-assertions': 'off'
     }
